050213cf6df753d82639c1ad7f3da8d74555cedd
Vinay Kumar Adiga <vinayadiga96@gmail.com>
2021-03-24 20:41:31 +0530

    login page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  NbCardModule,
+  NbThemeModule,
+  NbLayoutModule,
+  NbButtonModule,
+  NbIconModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, LoginPageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbEvaIconsModule,
+    NbCardModule,
+    NbLayoutModule,
+    NbButtonModule,
+    NbIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
